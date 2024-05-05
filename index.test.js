3b4e456bc067a26d0286fab1e5895438c08d5026
Lilsc0pe <163313783+NureMartynovBohdan@users.noreply.github.com>
2024-05-05 23:04:42 +0300

    test site

---
+++ index.test.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const app = require("./index.js");
+
+describe("GET /user/:id", () => {
+  it("should return user data for a valid ID", async () => {
+    const res = await request(app).get("/user/valid_id");
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("user_id");
+  });
+
+  it("should return 404 for an invalid ID", async () => {
+    const res = await request(app).get("/user/invalid_id");
+    expect(res.statusCode).toEqual(404);
+  });
+
+  it("should return 404 for a nonexistent route", async () => {
+    const res = await request(app).get("/nonexistent_route");
+    expect(res.statusCode).toEqual(404);
+  });
+});
